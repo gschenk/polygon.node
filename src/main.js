6d130a1b901ba0ca0ddfb52ca8e6e445f3b52834
gerald schenk <gschenk@gmx.net>
2020-03-28 02:49:59 +0100

    adds input filtering, unique points for N<3000

--- src/main.js
+++ src/main.js
@@ -5,16 +5,10 @@ const Extremes = require('./extremes');
 // const Polygon = require('./polygon');
 
 // constant parameters
-const BIGN = 1e9;
+const BIGN = 3000;
 
 // sandbox area
 // const myPolygon = new Polygon(0, [myPoint]);
-
-
-// convexHullArea :: [Point] -> Double
-function convexHullArea(nodes) {
-  return 0; // stub
-}
 
 // spaces :: Primitive -> String
 const spaces = x => ' '.repeat(28 - `${x}`.length);
@@ -22,9 +16,22 @@ const spaces = x => ' '.repeat(28 - `${x}`.length);
 // xyTable :: Point -> String
 const xyTable = point => console.log(`${point.id}   ${point.x}${spaces(point.x)}${point.y}`);
 
+
+
+// convexHullArea :: [Point] -> Double
+function convexHullArea(nodes) {
+  return 0; // stub
+}
+
 // parseJsonPoints :: String -> Object
-const parseJsonPoints = s => JSON.parse(s)
-  .map((a, i) => new Point(i, a.x, a.y));
+const parseJsonPoints = s => {
+  const data = JSON.parse(s)
+  // remove duplicates for small sets of data points
+  const uniqueData = data.length > BIGN
+    ? data
+    : [...new Set(data.map(o => JSON.stringify(o)))].map(t => JSON.parse(t));
+  return uniqueData.map((a, i) => new Point(i, a.x, a.y));
+};
 
 // read input from STDIN
 const readline = new ReadlineWrapper().stdin;
